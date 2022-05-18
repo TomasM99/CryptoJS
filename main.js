3dddb8313e7952e17584ec73f04b809ac2d9a252
Tomas Margio <tomasmr99@gmail.com>
2022-05-18 00:51:49 -0300

    Agrego primer desafio obligatorio

---
+++ main.js
@@ -0,0 +1,80 @@
+//Compra-Venta de cripto
+
+let pesos = parseFloat(prompt("Ingrese la cantidad de dinero que tiene"));
+let cripto = 0;
+let menu;
+
+function saldo(pesos, cripto){
+    alert("Su nuevo saldo es de: $" + pesos + " y " + cripto + " criptomonedas");
+}
+
+do {
+
+    menu = parseFloat(prompt("Para comprar cripto ingrese: 1\nPara vender cripto ingrese: 2\nPara ver su saldo ingrese: 3\nPara cargar dinero ingrese: 4\nPara retirar dinero ingrese: 5\nPara salir ingrese: 0"));
+    
+    switch(menu){
+        case 1:
+
+            let compra = parseFloat(prompt("Ingrese cuanto quiere comprar"));
+
+            if(compra <= pesos){
+                pesos -= compra;
+                cripto = cripto + (compra * 0.5);
+                saldo(pesos,cripto);
+            }else{
+                alert("No tiene dinero suficiente");
+            }
+
+        break;
+
+        case 2:
+
+            let venta = parseFloat(prompt("Ingrese cuentas criptomonedas quiere vender"));
+
+            if(venta <= cripto){
+                cripto -= venta;
+                pesos = pesos + (venta * 2);
+                saldo(pesos,cripto);
+            }else{
+                alert("No tiene criptomonedas suficientes");
+            }
+
+        break;
+
+        case 3:
+
+            saldo(pesos,cripto);
+
+        break;
+
+        case 4:
+
+            let carga = parseFloat(prompt("Ingrese cantidad de dinero a cargar"));
+            pesos += carga;
+
+        break;
+
+        case 5:
+        
+            let retiro = parseFloat(prompt("Ingrese cantidad de dinero a retirar"));
+
+            if(retiro <= pesos){
+                pesos -= retiro;
+            }else{
+                alert("No posee esa cantidad de dinero");
+            }
+
+        break;
+
+        case 0:
+        break;
+
+        default:
+            alert("Ingrese un valor valido");
+        break;
+    }
+    
+
+
+
+} while (menu != 0);
